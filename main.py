7f15dc9445f5dc4058a6ccdac79478ab90d0ec63
Steve <87497042+sthermon@users.noreply.github.com>
2022-02-22 18:33:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,18 @@ st.write('##')
 st.write('With just few steps identify the probability to land you new next job')
 
 with st.form('job_description'):
-  st.text_area(label='Job description:', placeholder=''
+  st.text_area(label='Job description:', height=200, 
+               placeholder='''
+               Paste a job description here you wish to analyze.
+               
+               For example, paste qualifications, responsibilities, highlights 
+               but exclude information about the company (About Us), or benefits.
+               '''
+              )
+  st.form_submit_button('Analyze')
+  
+ with st.container():
+  st.file_uploader(label='Resume', accept_multiple_files=False, on_change='upload')
+  
+ st.write('---')
+
