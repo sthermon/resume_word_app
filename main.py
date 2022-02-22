f0e6ed640cec211441f33d5f7387949c25e2166c
Steve <87497042+sthermon@users.noreply.github.com>
2022-02-22 18:21:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 
+st.title('Improve your resume by tailoring it to a job offer')
+st.write('##')
+
+st.write('With just few steps identify the probability to land you new next job')
+
+with st.form('job_description'):
+  st.text_area(label='Job description:', placeholder=''
