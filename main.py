91c2e12ce015834134bb8e03dd4bb107b2e9c620
Steve <87497042+sthermon@users.noreply.github.com>
2022-03-28 23:31:28 -0400

    Finalize main file with streamlit application
    Code formatted and reorganized to run with compatibility with streamlit

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-import word_selector
-from word_selector import top_words
+from word_selector import clean_file, filter_text
+from word_selector import top_frequent_words
 import streamlit as st
 from streamlit_lottie import st_lottie
 import requests
 from pdf2docx import parse
 import docx
+import copy
 
-# Page description, icon and layout
-st.set_page_config(page_title='Job fit 4 me', page_icon='📃', layout='centered')
 
 # function to animate upload icon
-
-
 def animate_icon(url):
     r = requests.get(url)
     if r.status_code != 200:
@@ -19,127 +16,132 @@ def animate_icon(url):
     return r.json()
 
 
-@st.cache
-def load_description(text):
-    data = ''
-    with open('job_desc.txt', w) as job:
-        job.write(text)
-    pass
-
-# send_data = word_selector.filter_text()
+# Initialize session to keep cached returned fuction
+if 'text' not in st.session_state:
+    st.session_state.text = ''
 
 
 @st.cache
-def load_resume():
-    # pdfreader = pdfreaderfile(file)
-    # all_page_text = ''
-    # for i in range(count):
-    #     page = pdfreader.getpage(i)
-    #     all_page_text += page.extracttext()
-    # return all_page_text
-    pass
+def doc_reader(file):
+    doc = docx.Document(file)
+    pages = [p.text for p in doc.paragraphs]
+    parse_doc = str(pages)
+    return parse_doc
+
+
+@st.cache
+def pass_file(txt):
+    with open('text_transc.txt', 'w', encoding='utf-8') as f:
+        f.write(txt)
 
 
 # icon from https://lottiefiles.com/
 lottie_icon = animate_icon(
     'https://assets2.lottiefiles.com/packages/lf20_ir6xNQ.json')
 
-title_1, title_2, title_3 = st.columns([1, 2, 1])
-with title_1:
-    st.empty()
-with title_2:
-    st.title('Job fit 4 me')
-with title_3:
-    st.empty()
+def main():
 
-st.write('##')
-st.header('Improve your resume by targeting it to a job offer')
-st.write('##')
+    # Page description, icon and layout
+    st.set_page_config(page_title='Job fit 4 me', page_icon='📃', layout='centered')
+    title_1, title_2, title_3 = st.columns([1, 2, 1])
+    with title_1:
+        st.empty()
+    with title_2:
+        st.title('Job fit 4 me')
+    with title_3:
+        st.empty()
+    st.write('---')
 
+    st.write('##')
+    st.header('Improve your resume by targeting it to a job offer in just 3 steps')
+    st.write('##')
 
-# container to process text for job description and handle with function from word selector
-with st.container():
-    st.subheader(
-        'Analyze a job description and identify important skills and keywords:')
+    st.write('##')
 
-    with st.form(key='job_desc', clear_on_submit=True):
-        descr = st.text_area(label='Job description:', height=250,
-                             placeholder='''Paste a job description here you wish to analyze its skills. 
+    # container to process text for job description and handle with function from word selector
+    with st.container():
 
-For example, paste qualifications, responsibilities, highlights 
-but exclude information about the company (About us, diversity, or benefits) as it will reduce probability.
-Job descriptions in English only...'''
-                             )
-        send_desc = st.form_submit_button('Analyze')
-        if not send_desc:
-            st.info('Please enter a job description')
-            # st.stop()
-        if send_desc:
-            st.spinner('Processing data...')
-            data = load_description(send_desc)
-            st.success('Data sent!')
-            # if animation can be added to compare text and doc: https://assets2.lottiefiles.com/private_files/lf30_1j94r4tx.json
+        with st.form(key='form', clear_on_submit=False):
+            st.write('##')
+            st.subheader(
+                'Analyze a job offer by entering it to the text field:')
+            descr = st.text_area(label='Job description:', height=250, key='text',
+                                placeholder='''Paste a job description here you wish to analyze its skills. 
 
-st.write('##')
-st.write('---')
-# container to uploaded file and further handling
-with st.container():
-    st.subheader(
-        'Compare your resume against this job offer to see your chances of being considered')
+    For example, paste qualifications, responsibilities, highlights 
+    but exclude information about the company (About us, diversity, or benefits) as it will reduce probability.
+    Job descriptions in English only...'''
+                                )
+            st.subheader(
+                'Compare your resume against this job offer to see your match percentage to be selected by a system')
+            pass_file(descr)
+            st.write('##')
+            mid_col, right_col = st.columns([2, 2])
+            with mid_col:
+                doc_file = st.file_uploader('Please submit .docx or .pdf files only', key='file', accept_multiple_files=False,
+                                            help='Drag and drop your file or hit "Browse files"', type=['pdf', 'docx'])
+                if doc_file is not None:
+                    if doc_file.type == 'application/pdf':
+                        file_details = {
+                            'Filename': doc_file.name, 'Filesize': doc_file.size, 'Filetype': doc_file.type}
+                        # ******************* optional *********************************
+                        st.write(file_details)
+                        try:
+                            pdf_file = doc_file
+                            new_file = 'doc_from_pdf.docx'
+                            parse(pdf_file=pdf_file,
+                                docx_file=new_file, start=0, end=None)
+                            result = doc_reader(new_file)
+                        except (TypeError):
+                            st.warning('Unable to read file')
+                    else:
+                        result = doc_reader(doc_file)
+                    with right_col:
+                        st_lottie(lottie_icon, height=150, key='icon')
+                    pass_file(result)
 
-    mid_col, right_col = st.columns([2, 2])
-    with mid_col:
-        doc_file = st.file_uploader('', accept_multiple_files=False,
-                                    help='Only .docx or .pdf files', type=['pdf', 'docx'], )
-        st.markdown('Drag and drop your file or hit "Browse files"')
-    with right_col:
-        st_lottie(lottie_icon, height=200, key='doc')
+            st.write('##')
+            submit = st.form_submit_button(
+                'Analyze')
+            if not submit:
+                st.info('Upload your resume and click Analyze to continue')
+            if submit:
+                st.spinner('Processing data...')
+    st.write('---')
+    if st.session_state['FormSubmitter:form-Analyze'] is not False:
+        with st.container():
+            st.subheader('Match percentage')
+            description = filter_text(descr)
+            resume = clean_file('text_transc.txt')
+            # https://www.geeksforgeeks.org/python-percentage-similarity-of-lists/?ref=gcse
+            match = len(set(resume) & set(description)) / \
+                float(len(set(resume) | set(description))) * 100
+            st.write(match)
+            st.caption(
+                'An ideal percentage should be close to 70%, a higher percentage will also decrease the chances of being selected by a system')
 
-    if not doc_file:
-        st.info('Upload your resume in .pdf or .docx format only')
-        # st.stop()'''
-    if doc_file is not None:
-        if doc_file.type == 'application/pdf':
-            pdf_file = doc_file
-            new_file = 'doc_from_pdf.docx'
-            parse(pdf_file='pdf_file', docx_file='new_file', start=0, end=None)
-        else:
-            doc = docx.Document(doc_file)
-            pages = [p.text for p in doc.paragrahps]
-            parse_doc = str(pages)
+        # dropdown selection to show the top 10 and top 20 most common words found in the offer
+        with st.container():
+            st.subheader('Top words of your analysis')
+        with st.expander('Job description'):
+            descript_1 = copy.deepcopy(description)
+            top_words = list(top_frequent_words(descript_1))
+            top_30 = top_words[:30]
+            st.write(top_30)
+            
+        with st.expander('Resume'):
+            resum_1 = copy.deepcopy(resume)
+            top_words2 = list(top_frequent_words(resum_1))
+            
+            top_30_1 = top_words2[:30]
+            st.write(top_30_1)
 
-st.write('---')
+        # Clear cached items and functions to prepare for new usage
+        new_analysis = st.button('New analysis')
+        if new_analysis:
+            for key in st.session_state.keys():
+                del st.session_state[key]
+                st.experimental_memo.clear()
 
-
-show_match = st.button('Show match')
-if not show_match:
-    st.stop()
-    st.success('success')
-
-# TODO Use pandas or matplotlib to visualize the similarities found for resume and offer
-
-with st.container():
-    st.subheader('Match percentage')
-    st.write('plot pie char here')
-    st.caption(
-        'An ideal percentage should be close to 70%, higher changes will decrease also the chances of being selected')
-
-
-# dropdown selection to show the top 10 and top 20 most common words found in the offer
-with st.container():
-    st.subheader('Select your top words')
-
-    top_25 = (top_words[:25])
-    top_10 = (top_words[:10])
-    top_5 = (top_words[:5])
-
-    top_word_select = st.selectbox(
-        'Pick one option', ['None', 'Top 5', 'Top 10', 'Top 25'])
-    if top_word_select == 'Top 5':
-        st.write(top_5)
-    if top_word_select == 'Top 10':
-        st.write(top_10)
-    if top_word_select == 'Top 25':
-        st.write(top_25)
-    else:
-        ''
+if __name__ =='__main__':
+    main()
